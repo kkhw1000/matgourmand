f60556f485e0d9884191c6c3c3f202c03ec0894a
kkhw1000 <kkhw1000@naver.com>
2026-06-29 23:06:36 +0900

    Initial commit

---
+++ src/main/java/com/matgourmand/MatGourmandApplication.java
@@ -0,0 +1,13 @@
+package com.matgourmand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatGourmandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatGourmandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matgourmand/MatGourmandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matgourmand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatGourmandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
